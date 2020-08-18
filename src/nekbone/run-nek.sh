df6f4e044db3fc2b19fc60a9fea8d8990ee5985b
Samuel Pollard <spollard@cs.uoregon.edu>
2020-08-17 20:38:54 -0700

    Version for paper experiments, if they can get that far

--- src/nekbone/run-nek.sh
+++ src/nekbone/run-nek.sh
@@ -15,14 +15,14 @@ cd "test/example1/"
 LP=288
 LELT=9216 # 72 * 128
 
-NUM_TRIALS=50
-NP_COUNT="16 72"
+NUM_TRIALS=100
+NP_COUNT="72"
 ALLREDUCE_ALGOS="default ompi mpich mvapich2 impi rab1 rab2 rab_rsag rdb smp_binomial smp_binomial_pipeline smp_rdb smp_rsag smp_rsag_lr smp_rsag_rab redbcast ompi_ring_segmented mvapich2_rs mvapich2_two_level rab"
 # Algorithms that don't work in this case (and thus change to default):
 # lr automatic
 
 # Some necessary janitorial code. SIZE contains compile-time constants
-TRIAL_FMT=$(seq --format='%03.0f' 1 $NUM_TRIALS)
+TRIAL_FMT=$(seq --format='%03.0f' 19 $NUM_TRIALS)
 LELG=$(echo "$LP * $LELT" | bc)
 cp ../../custom/SIZE .
 
@@ -35,11 +35,20 @@ for TRIAL in $TRIAL_FMT; do
 		IEL0=$(echo "$LELT / $NP" | bc)
 		IELN=$(echo "$IEL0 * 2 + 1" | bc)
 		sed "s/400 400 1 = iel0,ielN/$IEL0 $IELN $IEL0 = iel0,ielN/g" ../../custom/data.rea > data.rea
-		# TODO: Maybe want to change polynomial order in data.rea too?
+		# Another tunable factor is the polynomial order in data.rea
 
-		for ALLREDUCE in $ALLREDUCE_ALGOS; do
-			make -f ../../custom/exp-Makefile TRIAL="$LELT-$TRIAL" ALLREDUCE="$ALLREDUCE" "np-$NP"
-		done
+		# Loop over the topologies that make sense for a given processor count.
+		if [ "$NP" -eq 72 ]; then
+			make -j -f ../../custom/exp-Makefile TRIAL="$LELT-$TRIAL" TOPO="fattree-72" NP=$NP all
+			# Just run one topology for now; these experiments take a while
+			# make -j -f ../../custom/exp-Makefile TRIAL="$LELT-$TRIAL" TOPO="torus-2-4-9" NP=$NP all
+		elif [ "$NP" -eq 16 ]; then
+			make -j -f ../../custom/exp-Makefile TRIAL="$LELT-$TRIAL" TOPO="fattree-$NP" NP=$NP all
+			make -j -f ../../custom/exp-Makefile TRIAL="$LELT-$TRIAL" TOPO="torus-2-2-4" NP=$NP all
+		else
+			echo "Bad topology for np = $NP"
+			exit 1
+		fi
 	done
 done
 cd -
